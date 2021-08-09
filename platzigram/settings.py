50c329511babad086eb9d0738674aeb1287c7dc6
Andres <hir.islas@outlook.com>
2021-08-09 18:14:29 -0500

    hidden secret key

--- platzigram/settings.py
+++ platzigram/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from secret_settings import secret_key_hidden
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=k$b$in)hcqmx1y^g%_wtexk&rd2shd*w)e#vy#v*nyuj2*y)w'
+SECRET_KEY = secret_key_hidden
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
