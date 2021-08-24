df0635df66ab577bf453d60135739b8c97130283
Andres <hir.islas@outlook.com>
2021-08-23 23:36:21 -0500

    Add authentication models to create new profile, Auth Custom

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -13,8 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+#Django
 from django.contrib import admin
 from django.urls import path
+
 from platzigram import views as local_views
 from posts import views as post_views
 
